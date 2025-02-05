f21d4a5637a4f05e0bdfe28629aa936bf72fabae
thomasavada <anhnt@avada.io>
2025-02-06 01:32:36 +0700

    Add firebase

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  output: 'export',
   images: {
+    unoptimized: true, // Add this line
     remotePatterns: [
       {
         protocol: "https",
