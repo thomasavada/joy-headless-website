005a7b101f64fc759467fd85f2f9feb4c616c833
thomasavada <anhnt@avada.io>
2025-02-09 18:19:09 +0700

    Update dark mode

--- components/layout/theme-provider.tsx
+++ components/layout/theme-provider.tsx
@@ -1,9 +1,33 @@
 "use client";
 
-import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  
+  useEffect(() => {
+    // Set theme based on path
+    if (pathname.startsWith('/blog')) {
+      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.add('light');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.documentElement.classList.remove('light');
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  }, [pathname]);
+
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme={pathname.startsWith('/blog') ? 'light' : 'dark'}
+      enableSystem={false}
+      forcedTheme={pathname.startsWith('/blog') ? 'light' : 'dark'} // Force theme based on path
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
