9708425ae251f3393b56a4414b7976f22f789045
thomasavada <anhnt@avada.io>
2025-02-25 12:09:59 +0700

    Update meta

--- app/pricing/metadata.ts
+++ app/pricing/metadata.ts
@@ -1,6 +1,11 @@
-import {Metadata} from "next";
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Pricing - Joy",
-  description: "Choose the perfect plan for your business",
+  title: 'A customized pricing plan for every business size - Joy',
+  description: 'Our pricing plans are tailored to meet the unique needs of businesses of all sizes, ensuring flexibility and value at every stage of growth.',
+  openGraph: {
+    title: 'A customized pricing plan for every business size - Joy',
+    description: 'Our pricing plans are tailored to meet the unique needs of businesses of all sizes, ensuring flexibility and value at every stage of growth.',
+    type: 'website',
+  },
 };
